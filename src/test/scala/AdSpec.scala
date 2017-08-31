ec137d0daea154d0e23cb1329230913518fd890a
fvlankvelt <frank@vanlankvelt.net>
2017-08-31 09:39:26 +0200

    evaluate nodes in test as well

--- src/test/scala/AdSpec.scala
+++ src/test/scala/AdSpec.scala
@@ -10,6 +10,8 @@ class AdSpec extends FlatSpec {
   "AD" should "calculate scalar derivative" in {
     val x = Var[Id, Double](2.0)
     val y = x * x
+    assert(y() == 4.0)
+
     val z: Double = y.grad(x)
     assert(z == 4.0)
   }
@@ -17,6 +19,8 @@ class AdSpec extends FlatSpec {
   it should "calculate vector derivative on List" in {
     val x = Var[List, Double](List(1.0, 2.0))
     val y = x * x
+    assert(y() == List(1.0, 4.0))
+
     val z = y.grad(x)
     assert(z == List(List(2.0, 0.0), List(0.0, 4.0)))
   }
@@ -24,6 +28,8 @@ class AdSpec extends FlatSpec {
   it should "calculate derivative of a scalar function" in {
     val x = Var[Id, Double](2.0)
     val y = pp.log(x)
+    assert(y() == scala.math.log(2.0))
+
     val z: Double = y.grad(x)
     assert(z == 0.5)
   }
@@ -31,6 +37,8 @@ class AdSpec extends FlatSpec {
   it should "calculate derivative of a function applied to a list" in {
     val x = Var[List, Double](List(1.0, 2.0))
     val y = pp.log(x)
+    assert(y() == List(0.0, scala.math.log(2.0)))
+
     val z = y.grad(x)
     assert(z == List(List(1.0, 0.0), List(0.0, 0.5)))
   }
