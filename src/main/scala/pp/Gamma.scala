3773bf83511083cbeb31318d24edf1009f8fd63b
fvlankvelt <frank@vanlankvelt.net>
2017-09-05 17:49:44 +0200

    remove obsolete implementation

--- src/main/scala/pp/Gamma.scala
+++
@@ -1,7 +0,0 @@
-package pp
-
-case class Gamma(alpha: ScalarVariableLike, beta: ScalarVariableLike) extends ScalarDistribution {
-  import Function._
-
-  override def logp() = alpha * log(beta) + (alpha - 1.0f) * log(this) -beta * this - lgamma(alpha)
-}
